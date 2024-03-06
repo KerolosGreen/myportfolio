0fb4922d0f98cb2d185241750e5b0aae1c2359ed
Kerolos Safwat <92627756+KerolosGreen@users.noreply.github.com>
2024-03-06 17:57:48 +0200

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,7 +7,9 @@ function Home(){
             <img data-aos="fade-down" data-aos-delay="150" data-aos-once="true" className='gifball' src='../../public%20assets/circle.gif'/>
             <h1 data-aos="fade-right" data-aos-delay="150" data-aos-once="true">Hi , I'm Kerolos Safwat</h1>
             <p data-aos="fade-left" data-aos-delay="150" data-aos-once="true">And This Is My Portfolio</p>
+            <a href="../../../public/public assets/Kerolos Safwat maher Cv PL.pdf">
             <button data-aos="zoom-out" data-aos-delay="150" data-aos-once="true" className='resume-button'>My Resume</button>
+            </a>
             <Work/>
         </div>
     )
