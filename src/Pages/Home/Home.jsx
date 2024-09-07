e7750510d56845f10f8c380056fde028dbe87938
Kerolos Safwat <92627756+KerolosGreen@users.noreply.github.com>
2024-09-07 07:05:02 +0300

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,10 +7,12 @@ function Home(){
             <img data-aos="fade-down" data-aos-delay="150" data-aos-once="true" className='gifball' src='../../public%20assets/circle.gif'/>
             <h1 data-aos="fade-right" data-aos-delay="150" data-aos-once="true">Hi , I'm Kerolos Safwat</h1>
             <p data-aos="fade-left" data-aos-delay="150" data-aos-once="true">And This Is My Portfolio</p>
-            <img data-aos="fade-down" data-aos-delay="150" data-aos-once="true" src="https://raw.githubusercontent.com/KerolosGreen/myportfolio/main/public/public%20assets/Github-Kerolos_ReadmeImage.png" style={{width: '80%'}}/>
-{/*             <a href="https://raw.githubusercontent.com/KerolosGreen/myportfolio/main/public/public%20assets/Kerolos Safwat FullStack - Resume.pdf">
+            
+            <a href="https://raw.githubusercontent.com/KerolosGreen/myportfolio/main/public/public%20assets/Kerolos Safwat FullStack - Resume.pdf">
             <button data-aos="zoom-out" data-aos-delay="150" data-aos-once="true" className='resume-button'>My Resume</button>
-            </a> */}
+            </a>
+
+            <img data-aos="fade-down" data-aos-delay="150" data-aos-once="true" src="https://raw.githubusercontent.com/KerolosGreen/myportfolio/main/public/public%20assets/Github-Kerolos_ReadmeImage.png" style={{width: '80%'}}/>
             <Work/>
 {/*             <img src="https://raw.githubusercontent.com/KerolosGreen/myportfolio/main/public/public%20assets/Github-Kerolos_ReadmeImage.png" style={{width: '80%'}}/> */}
         </div>
