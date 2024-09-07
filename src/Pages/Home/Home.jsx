2626a96ea12fbdc38e669f7a2c5413159de2b24c
Kerolos Safwat <92627756+KerolosGreen@users.noreply.github.com>
2024-09-07 07:03:03 +0300

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,9 +3,8 @@ import Work from '../Work/Work'
 function Home(){
     return(
         <div className='Home'>
-            
-{/*             <img data-aos="fade-down" data-aos-delay="150" data-aos-once="true" className='gifball' src='../../public%20assets/circle.gif'/> */}
             <img data-aos="fade-down" data-aos-delay="150" data-aos-once="true" src="https://raw.githubusercontent.com/KerolosGreen/myportfolio/main/public/public%20assets/Github-Kerolos_ReadmeImage.png" style={{width: '80%'}}/>
+            <img data-aos="fade-down" data-aos-delay="150" data-aos-once="true" className='gifball' src='../../public%20assets/circle.gif'/>
             <h1 data-aos="fade-right" data-aos-delay="150" data-aos-once="true">Hi , I'm Kerolos Safwat</h1>
             <p data-aos="fade-left" data-aos-delay="150" data-aos-once="true">And This Is My Portfolio</p>
 {/*             <a href="https://raw.githubusercontent.com/KerolosGreen/myportfolio/main/public/public%20assets/Kerolos Safwat FullStack - Resume.pdf">
