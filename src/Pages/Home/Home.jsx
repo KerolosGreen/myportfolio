4e5e825847cf1ce940ebbd94adf358222fc39a0f
Kerolos Safwat <92627756+KerolosGreen@users.noreply.github.com>
2024-09-08 06:59:25 +0300

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -12,9 +12,10 @@ function Home(){
             <button data-aos="zoom-out" data-aos-delay="150" data-aos-once="true" className='resume-button'>My Resume</button>
             </a>
             <img className="backgroundpattern" ata-aos="fade-right" data-aos-delay="150" data-aos-once="true" src="https://raw.githubusercontent.com/KerolosGreen/myportfolio/387b4b4135db22881cd42168d2771fabc8d285b6/public/public%20assets/spiral.svg"/>
-            <img className="ReadMeImg" data-aos="fade-down" data-aos-delay="150" data-aos-once="true" src="https://raw.githubusercontent.com/KerolosGreen/myportfolio/main/public/public%20assets/linkedinimg4.png"/>
+{/*             <img className="ReadMeImg" data-aos="fade-down" data-aos-delay="150" data-aos-once="true" src="https://raw.githubusercontent.com/KerolosGreen/myportfolio/main/public/public%20assets/linkedinimg4.png"/> */}
             <img className="ReadMeImgMobile" data-aos="fade-down" data-aos-delay="150" data-aos-once="true" src="https://raw.githubusercontent.com/KerolosGreen/myportfolio/main/public/public%20assets/Portfolio%20Mobile%20ReadMEIMG-5.png"/>
             <Work/>
+             <img className="ReadMeImg" data-aos="fade-down" data-aos-delay="150" data-aos-once="true" src="https://raw.githubusercontent.com/KerolosGreen/myportfolio/main/public/public%20assets/linkedinimg4.png"/>
 {/*             <img src="https://raw.githubusercontent.com/KerolosGreen/myportfolio/main/public/public%20assets/Github-Kerolos_ReadmeImage.png" style={{width: '80%'}}/> */}
         </div>
     )
